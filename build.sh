7efb7aab67af88f715f7d28b8e2caf49efc1f97d
Hetsh <git@hetsh.de>
2020-08-25 13:05:45 +0200

    Added upload option

--- build.sh
+++ build.sh
@@ -10,6 +10,7 @@ cd "$CWD"
 
 # Load helpful functions
 source libs/common.sh
+source libs/docker.sh
 
 # Check access to docker daemon
 assert_dependency "docker"
@@ -20,39 +21,46 @@ fi
 
 # Build the image
 APP_NAME="teamspeak"
-APP_TAG="hetsh/$APP_NAME"
-docker build --tag "$APP_TAG" --tag "$APP_TAG:$(git describe --tags --abbrev=0)" .
+IMG_NAME="hetsh/$APP_NAME"
+docker build --tag "$IMG_NAME" --tag "$IMG_NAME:$(git describe --tags --abbrev=0)" .
 
 # Start the test
-if [ "${1-}" = "--test" ]; then
-	# Set up temporary directory
-	TMP_DIR=$(mktemp -d "/tmp/$APP_NAME-XXXXXXXXXX")
-	add_cleanup "rm -rf $TMP_DIR"
+case "${1-}" in
+	"--test")
+		# Set up temporary directory
+		TMP_DIR=$(mktemp -d "/tmp/$APP_NAME-XXXXXXXXXX")
+		add_cleanup "rm -rf $TMP_DIR"
 
-	# Configuration from ReadMe
-	echo "license_accepted=1
+		# Configuration from ReadMe
+		echo "license_accepted=1
 dbsqlpath=/opt/teamspeak-server/sql/
 logpath=/var/log/teamspeak-server
 serverquerydocs_path=/opt/teamspeak-server/serverquerydocs" > "$TMP_DIR/app.ini"
 
-	# Apply permissions, UID matches process user
-	extract_var APP_UID "./Dockerfile" "\K\d+"
-	chown -R "$APP_UID":"$APP_UID" "$TMP_DIR"
+		# Apply permissions, UID matches process user
+		extract_var APP_UID "./Dockerfile" "\K\d+"
+		chown -R "$APP_UID":"$APP_UID" "$TMP_DIR"
 
-	# Start the test
-	extract_var CONF_DIR "./Dockerfile" "\"\K[^\"]+"
-	extract_var DATA_DIR "./Dockerfile" "\"\K[^\"]+"
-	docker run \
-	--rm \
-	--tty \
-	--interactive \
-	--publish 9987:9987/udp \
-	--publish 10011:10011/tcp \
-	--publish 10022:10022/tcp \
-	--publish 30033:30033/tcp \
-	--mount type=bind,source="$TMP_DIR/app.ini",destination="$CONF_DIR/app.ini" \
-	--mount type=bind,source="$TMP_DIR",destination="$DATA_DIR" \
-	--mount type=bind,source=/etc/localtime,target=/etc/localtime,readonly \
-	--name "$APP_NAME" \
-	"$APP_NAME"
-fi
+		# Start the test
+		extract_var CONF_DIR "./Dockerfile" "\"\K[^\"]+"
+		extract_var DATA_DIR "./Dockerfile" "\"\K[^\"]+"
+		docker run \
+		--rm \
+		--tty \
+		--interactive \
+		--publish 9987:9987/udp \
+		--publish 10011:10011/tcp \
+		--publish 10022:10022/tcp \
+		--publish 30033:30033/tcp \
+		--mount type=bind,source="$TMP_DIR/app.ini",destination="$CONF_DIR/app.ini" \
+		--mount type=bind,source="$TMP_DIR",destination="$DATA_DIR" \
+		--mount type=bind,source=/etc/localtime,target=/etc/localtime,readonly \
+		--name "$APP_NAME" \
+		"$IMG_NAME"
+	;;
+	"--upload")
+		if ! tag_exists "$IMG_NAME"; then
+			docker push "$IMG_NAME"
+		fi
+	;;
+esac
