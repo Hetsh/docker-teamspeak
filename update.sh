ae54c200cd3e10f9be11981a7e3d1a473b9e3b21
Hetsh <git@hetsh.de>
2021-11-28 16:28:25 +0100

    Use the update_web function from libs

--- update.sh
+++ update.sh
@@ -19,21 +19,14 @@ assert_dependency "curl"
 # Alpine Linux
 update_image "amd64/alpine" "Alpine Linux" "false" "\d{8}"
 
-# Teamspeak Server
-TS_PKG="APP_VERSION"
-TS_VERSION_REGEX="(\d+\.)+\d+"
-CURRENT_TS_VERSION=$(cat Dockerfile | grep -P -o "$TS_PKG=\K$TS_VERSION_REGEX")
-NEW_TS_VERSION=$(curl --silent --location "https://files.teamspeak-services.com/releases/server/" | grep -P -o "$TS_VERSION_REGEX" | sort --version-sort | tail -n 1)
-if [ "$CURRENT_TS_VERSION" != "$NEW_TS_VERSION" ]; then
-	prepare_update "$TS_PKG" "Teamspeak Server" "$CURRENT_TS_VERSION" "$NEW_TS_VERSION"
-	update_version "$NEW_TS_VERSION"
-fi
-
 # Packages
 IMG_ARCH="x86_64"
 BASE_PKG_URL="https://pkgs.alpinelinux.org/package/edge"
 update_pkg "libstdc++" "Std. C++ Libs" "false" "$BASE_PKG_URL/main/$IMG_ARCH" "(\d+\.)+\d+_git\d{8}-r\d+"
 update_pkg "ca-certificates" "CA-Certificates" "false" "$BASE_PKG_URL/main/$IMG_ARCH" "\d{8}-r\d+"
+
+# Teamspeak Server
+update_web "APP_VERSION" "Teamspeak Server" "true" "https://files.teamspeak-services.com/releases/server" "(\d+\.)+\d+"
 
 if ! updates_available; then
 	#echo "No updates available."
